cc16342c30f18eee72e9c5f2d02c35d3d0f3b26c
kyle <kyle@wrtn.io>
2025-09-22 13:20:21 +0900

    fix

--- src/train.py
+++ src/train.py
@@ -141,14 +141,13 @@ def calculate_perplexity(
             # 패딩 토큰(0) 제외
             non_pad_mask = target_di != 0
             if non_pad_mask.sum() > 0:
-                logsoftmax = nn.LogSoftmax(dim=1)(decoder_output)
-                print(
-                    logsoftmax[non_pad_mask].shape,
-                    target_di[non_pad_mask].shape,
-                )
+                logsoftmax = nn.LogSoftmax(dim=1)
+                # print(
+                #     logsoftmax[non_pad_mask].shape,
+                #     target_di[non_pad_mask].shape,
+                # )
                 step_loss = criterion(
-                    logsoftmax[non_pad_mask],
-                    target_di[non_pad_mask],
+                    logsoftmax(decoder_output[non_pad_mask]), target_di[non_pad_mask]
                 )
                 print(step_loss)
                 batch_loss += step_loss.item() * non_pad_mask.sum().item()
