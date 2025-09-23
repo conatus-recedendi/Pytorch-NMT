8f2853cea26d4d0180957fdb4d6d9f64d7b36331
kyle <kyle@wrtn.io>
2025-09-23 09:48:48 +0900

    fix

--- src/train.py
+++ src/train.py
@@ -397,7 +397,13 @@ progress = 0.0
 avg_loss = 0.0
 total_batch_count = 0
 batch_size = args.batch_size
-print("max total_Batch_count: ", (len(pairs) // batch_size) * args.n_epochs)
+print(
+    "max total_Batch_count: ",
+    (len(pairs) // batch_size) * args.n_epochs,
+    len(pairs),
+    batch_size,
+    args.n_epochs,
+)
 for epoch in range(1, args.n_epochs + 1):
     # Get training data for this cycle
     if epoch > 5:
