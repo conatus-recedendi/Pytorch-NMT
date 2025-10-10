67d4b08b25301563185d79a472fd82de8ba95f5a
kyle <kyle@wrtn.io>
2025-10-10 18:33:12 +0900

    fix

--- src/etl.py
+++ src/etl.py
@@ -83,6 +83,7 @@ def tensor_from_sentence(lang, sentence, device="cpu", is_src=True):
     # print(sentence)
 
     max_len = 50
+    print(type(sentence), len(sentence), sentence)
     indexes = indexes_from_sentence(lang, sentence)
 
     if is_src:
