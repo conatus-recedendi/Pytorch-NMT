aef618eec1a487b3cebb179a9d5609fa00886839
kyle <kyle@wrtn.io>
2025-10-10 16:07:01 +0900

    fix

--- src/eval.py
+++ src/eval.py
@@ -222,7 +222,8 @@ def greedy_decode(decoder_context, decoder_hidden, encoder_outputs, max_len):
         # Choose top word from output
         topv, topi = decoder_output.data.topk(1)
         if di < 5:
-            print(decoder_output.data.topk(10))
+            # print(decoder_output.data.topk(10))
+            print(topi)
             print(output_lang.n_words)
         ni = topi.item()
         if ni == Language.eos_token:
