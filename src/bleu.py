97962130eef072395e88b9a100804e249cc58eb3
kyle <kyle@wrtn.io>
2025-10-16 07:02:16 +0900

    fix

--- src/bleu.py
+++ src/bleu.py
@@ -3,7 +3,8 @@ from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
 
 ref = [line.strip().split() for line in open("../rewrite/test.14.de", encoding="utf-8")]
 hyp = [
-    line.strip().split() for line in open("../test.14.hypothesis.de", encoding="utf-8")
+    line.strip().split()
+    for line in open("../test.14.hypothesis.id=10.de", encoding="utf-8")
 ]
 
 refs = [[r] for r in ref]  # 각 문장마다 참조 리스트
