cc06b3834f1ea8cf90427748bd40e13d669b9a26
kyle <kyle@wrtn.io>
2025-10-16 06:17:35 +0900

    fix

--- eval.sh
+++ eval.sh
@@ -16,7 +16,7 @@ python ./src/eval.py \
     --language de \
     --input_file ./rewrite/test.14.en \
     --input_ref_file ./rewrite/test.14.de \
-    --output_file test.14.hypothesis.de \
+    --output_file test.14.hypothesis.id=10.de \
     --max_len 50 \
     --beam_size 5 \
     --batch_size 1 \
