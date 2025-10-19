63b967135d3cb7a111557016c2b3c02fde20c218
kyle <kyle@wrtn.io>
2025-10-19 22:53:25 +0900

    fix

---
+++ train_id=16.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+
+
+# base + reverse + dropout + global + location + feed input
+
+# bash train_id=16.sh 1> base_id=16.log 2>/dev/tty
+
+OMP_NUM_THREADS=20 MKL_NUM_THREADS=20 python src/train.py \
+    --attn_model location \
+    --embedding_size 1000 \
+    --hidden_size 1000 \
+    --n_layers 4 \
+    --dropout 0.2 \
+    --teacher_forcing_ratio 1.0 \
+    --clip 5.0 \
+    --lr 1 \
+    --n_epochs 12 \
+    --plot_every 1 \
+    --print_every 1 \
+    --language de \
+    --device cuda \
+    --seed 19 \
+    --batch_size 128 \
+    --reverse True \ 
+    --feed_input True
