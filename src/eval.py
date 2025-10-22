7ef6ca53dba0c9a8ee72a2ea057ce632d68ef08f
kyle <kyle@wrtn.io>
2025-10-22 18:52:31 +0900

    fix

--- src/eval.py
+++ src/eval.py
@@ -306,14 +306,14 @@ def evaluate_file(input_file_path, input_ref_path, output_file_path=None, max_le
         # )
         results.append({"source": sentence, "greedy": greedy_translation})
         if idx % 100 == 0:  # Print samples every 100 sentences
-            print(f"{idx}/{len(sentences)} | latest avg loss: {loss / total_token:.4f}")
+            print(f"{idx}/{len(sentences)} | latest avg loss: {loss:.4f}")
             # print(f"src: {sentence}")
             # print(f"ref: {ref_sentences[idx]}")
             # print(f"beam: {beam_translation}")
             # print(f"greedy: {greedy_translation}")
             # print("=" * 50)
         idx += 1
-    loss /= total_token
+    # loss /= total_token
     print(f"Final Average Greedy Loss: {loss/len(sentences):.4f}")
     # print(f"Final Average Greedy Loss: {loss:.4f}")
     print(f"Final Greedy Perplexity: {math.exp(loss/len(sentences)):.2f}")
