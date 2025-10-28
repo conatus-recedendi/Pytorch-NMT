b4a8a7ddea01ebc3b688cd87a8f63dff6e4fdc52
kyle <kyle@wrtn.io>
2025-10-28 16:42:51 +0900

    fix

---
+++ eval_id=20.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# eval.sh
+# Copyright (C) 2018 LeonTao
+#
+# Distributed under terms of the MIT license.
+#
+
+
+python ./src/eval.py \
+    --run_id 20 \
+    --attn_model dot \
+    --embedding_size 1000 \
+    --hidden_size 1000 \
+    --n_layers 4 \
+    --dropout 0.2 \
+    --language de \
+    --input_file ./rewrite/test.14.en \
+    --input_ref_file ./rewrite/test.14.de \
+    --output_file test.14.hypothesis.id=20.de \
+    --max_len 50 \
+    --beam_size 12 \
+    --batch_size 1 \
+    --device cuda \
+    --seed 19 \
+    --reverse True \
+    --input_forward True
